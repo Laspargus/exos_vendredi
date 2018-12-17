0b9b3ad3358a64065d9db5ec22a685d5459bf2cc
Laspargus <maxime.speroni@gmail.com>
2018-12-17 13:35:26 +0100

    ajout exo 16

---
+++ exo_15.rb
@@ -0,0 +1,12 @@
+puts "quelle est ton année de naissance"
+naissance = gets.chomp.to_i
+age =2017-naissance
+
+i=0
+
+while naissance <= 2017 do
+    puts naissance
+    puts i
+    naissance=naissance + 1
+    i=i+1
+end
